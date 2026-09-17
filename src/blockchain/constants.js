6355adde99d0ec67f0a99d9224326139d1d82d73
NXTPAD <nxtpadsupport@gmail.com>
2026-09-17 08:05:47 -0500

    Create constants.js

---
+++ src/blockchain/constants.js
@@ -0,0 +1,14 @@
+export const BLOCKCHAIN_CONFIG = {
+  ethereum: {
+    chainId: 1,
+    name: "Ethereum Mainnet",
+    symbol: "ETH"
+  },
+
+  solana: {
+    name: "Solana Mainnet",
+    symbol: "SOL"
+  }
+};
+
+export const LAUNCH_FEE_USD = 1;
