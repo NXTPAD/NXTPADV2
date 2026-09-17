8073a0e8defb76e130f13772df79187a55a5bb30
NXTPAD <nxtpadsupport@gmail.com>
2026-09-17 07:38:44 -0500

    Create config.js

---
+++ src/src/config.js
@@ -0,0 +1,25 @@
+export const APP_CONFIG = {
+  name: "NXT PAD",
+
+  networks: {
+    ethereum: {
+      name: "Ethereum",
+      symbol: "ETH",
+      chainId: 1
+    },
+
+    solana: {
+      name: "Solana",
+      symbol: "SOL"
+    }
+  },
+
+  launchFee: {
+    usd: 1
+  },
+
+  treasury: {
+    ethereum: "",
+    solana: ""
+  }
+};
